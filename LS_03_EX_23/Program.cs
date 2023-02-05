ad2214a5cf63ec29e9ecb8f60e0c80467e1f0d9e
mvdudin <mdudin73@gmail.com>
2023-02-05 22:51:42 +0400

    C# ДЗ 3

---
+++ LS_03_EX_23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите целое число N: ");
+int n = int.Parse(Console.ReadLine()!);
+int i = 1;
+Console.WriteLine($"Таблица кубов чисел от 1 до {n}");
+while(i <= n)
+  { 
+   Console.Write(Math.Pow(i, 3));
+   if (i<n){Console.Write(", ");}
+   i+=1;
+  }  
+  Console.WriteLine();
