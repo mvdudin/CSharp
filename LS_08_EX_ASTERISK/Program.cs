0f43963d181b1a281cb5e892df7916dce1afe7b9
mvdudin <mdudin73@gmail.com>
2023-02-21 00:02:40 +0400

    C# ДЗ 8

---
+++ LS_08_EX_ASTERISK/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать функцию Sqrt(x) - квадратного корня, которая принимает на вход целочисленное значение x и 
+// возвращает целую часть квадратного корня от введенного числа
+
+int IntSqrt(int x , int k)
+{if (x < k)
+    {return 0;}
+  else
+    { return (1 + IntSqrt(x - k, k + 2));}
+}
+ 
+
+Console.Clear();
+Console.WriteLine(IntSqrt(256, 1));
